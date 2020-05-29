58a10e26849361ab09e0309ff9003682ee7f3324
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-05-29 14:13:36 +0300

    kurssitiedot step 7 ja 8

--- osa2/kurssitiedot/src/components/Course.js
+++ osa2/kurssitiedot/src/components/Course.js
@@ -3,7 +3,10 @@ import React from 'react'
 const Course = (props) => {
   return  (<div>
   <Header course={props.course} />
-  <Content parts={props.course.parts} /></div>
+  <Content parts={props.course.parts} />
+  <Total parts={props.course.parts} />
+  </div>
+
   )
 }
 const Header = (props) =>{
@@ -31,5 +34,16 @@ const Part = (props) =>{
         <> {props.part.name} {props.part.exercises}</>
     )
 }
-  
+const Total = (props) =>{
+    let total = props.parts.reduce((sum, part) =>{
+        return sum + part.exercises
+    }, 0
+    )
+    
+    return(
+      <div>
+        <p>total of {total} exercises</p>
+      </div>
+    )
+   } 
 export default Course
