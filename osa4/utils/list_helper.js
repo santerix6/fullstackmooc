e13dbc685fc5eca2384134337d94fe24589881d1
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-06-16 14:46:40 +0300

    4.5

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -9,8 +9,31 @@ const totalLikes = (array) => {
       return total + curr.likes
     }, 0)
   }
-
+}
+const favoriteBlog = (array) => {
+  if(array.length === 0){
+    return 0
+  } else {
+    var favo = 0
+    var favoindex = 0
+    for(var i = 0;i < array.length; i++){
+      if(array[i].likes > favo){
+        favo = array[i].likes
+        favoindex = i
+      }
+    }
+    return array[favoindex]
+  }
+  var favo = 0
+  var favoindex = 0
+  for(var i = 0;i < array.length; i++){
+    if(array[i].likes > favo){
+      favo = array[i].likes
+      favoindex = i
+    }
+  }
+  return array[favoindex]
 }
 module.exports = {
-  dummy, totalLikes
+  dummy, totalLikes, favoriteBlog
 }
