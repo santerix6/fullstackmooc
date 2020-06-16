25c9ca9d28bb4db1aa882660c384e98bf0b17b16
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-06-16 14:28:04 +0300

    4.4

--- osa4/utils/list_helper.js
+++ osa4/utils/list_helper.js
@@ -1,7 +1,16 @@
 const dummy = (blogs) => {
   return 1
 }
+const totalLikes = (array) => {
+  if(array.length === 0){
+    return 0
+  } else {
+    return array.reduce((total, curr) => {
+      return total + curr.likes
+    }, 0)
+  }
 
+}
 module.exports = {
-  dummy
+  dummy, totalLikes
 }
