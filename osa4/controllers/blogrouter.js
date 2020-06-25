fe8d9d8e2acb4f1db9b036ce8327cc6f276dbd5e
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2020-06-25 18:21:50 +0300

    4.19 ja fixauksii aikasempii

--- osa4/controllers/blogrouter.js
+++ osa4/controllers/blogrouter.js
@@ -2,30 +2,51 @@ const blogRouter = require('express').Router()
 const Blog = require('C:/Users/Santeri/fullstack/fullstackmooc/osa4/models/blog.js')
 const logger = require('C:/Users/Santeri/fullstack/fullstackmooc/osa4/utils/logger.js')
 const User = require('C:/Users/Santeri/fullstack/fullstackmooc/osa4/models/user.js')
+const jwt = require('jsonwebtoken')
 
+const getToken = (req) => {
+  const auth = req.get('authorization')
+  logger.info(auth)
+  if(auth && auth.toLowerCase().startsWith('bearer ')) {
+    return auth.substring(7)
+  }
+  return null
+}
 blogRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({}).populate('user')
+    const blogs = await Blog.find({}).populate('user', {id:1, name:1, username:1})
     response.json(blogs.map(blog => blog.toJSON()))
 })
 blogRouter.post('/', async (request, response) => {
   logger.info(request.body)
-  const user = await User.find().sort({ _id: -1 }).limit(1)
-  const blog = new Blog({
-    title : request.body.title,
-    author : request.body.author,
-    likes : request.body.likes,
-    url : request.body.url,
-    user : user[0]
-  })
+
+  const token = getToken(request)
   try {
-    const savedBlog = await blog.save()
-    user[0].blogs = user[0].blogs.concat(savedBlog._id)
-    await user[0].save()
-    response.json(savedBlog.toJSON())
+    const decodedToken = await jwt.verify(token, process.env.SECRET)
+    if( !token || !decodedToken.id){
+      return response.status(401).json({error : 'token missing or invalid'})
+    }
+    const user = await User.findById(decodedToken.id)
+    const blog = new Blog({
+      title : request.body.title,
+      author : request.body.author,
+      likes : request.body.likes,
+      url : request.body.url,
+      user : user._id
+    })
+    try {
+      const savedBlog = await blog.save()
+      user.blogs = user.blogs.concat(savedBlog._id)
+      await user.save()
+      response.json(savedBlog.toJSON())
+    } catch(error){
+      logger.error(error)
+      response.status(400).end()
+    }
   } catch(error){
-    logger.error(error)
-    response.status(400).end()
-  }
+      logger.error(error)
+      response.status(401).json({error : 'invalid token'})
+    }
+
 })
 blogRouter.delete('/:id', async (req, res) => {
   try {
--- osa4/controllers/userrouter.js
+++ osa4/controllers/userrouter.js
@@ -31,7 +31,7 @@ userRouter.post('/', async(req, res) => {
 })
 
 userRouter.get('/', async(req, res) => {
-  const users = await User.find({}).populate('blogs')
+  const users = await User.find({}).populate('blogs', {url :1, title:1, author:1,id:1})
   res.json(users.map(user => user.toJSON()))
 })
 
