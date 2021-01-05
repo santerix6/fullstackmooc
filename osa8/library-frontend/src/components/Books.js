50d143d865d1bcb9d5454f9607882a6aa0c104b3
Santeri Nuotiomaa <santeri.nuotiomaa@tuni.fi>
2021-01-06 00:41:21 +0200

    8.19

--- osa8/library-frontend/src/components/Books.js
+++ osa8/library-frontend/src/components/Books.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {useQuery} from '@apollo/client'
 import {ALL_BOOKS} from '../queries'
 
 
 const Books = (props) => {
+  const [genre, setGenre] = useState('allgenres')
   const res = useQuery(ALL_BOOKS, {
     pollInterval: 2000
   })
@@ -12,13 +13,22 @@ const Books = (props) => {
   }
   console.log(res);
   const books = res.data.allBooks
+  const genress = []
   if (!props.show) {
     return null
   }
+  for(var x of books){
+    for(var y of x.genres) {
+      if(genress.includes(y) === false){
+        genress.push(y)
+      }
+    }
+  }
+  genress.push("allgenres")
+  console.log(genress);
   return (
     <div>
       <h2>books</h2>
-
       <table>
         <tbody>
           <tr>
@@ -30,15 +40,32 @@ const Books = (props) => {
               published
             </th>
           </tr>
+          {genre==="allgenres" ?
+            books.map(a =>
+              <tr key={a.title}>
+                <td>{a.title}</td>
+                <td>{a.author.name}</td>
+                <td>{a.published}</td>
+              </tr>
+            )
+            :
+            null
+          }
           {books.map(a =>
+            a.genres.includes(genre)?
             <tr key={a.title}>
               <td>{a.title}</td>
-              <td>{a.autholocalr}</td>
+              <td>{a.author.name}</td>
               <td>{a.published}</td>
             </tr>
+            :
+            null
           )}
         </tbody>
       </table>
+      {genress.map(x =>
+        <button onClick={() => setGenre(x)} key={x}>{x}</button>
+      )}
     </div>
   )
 }
